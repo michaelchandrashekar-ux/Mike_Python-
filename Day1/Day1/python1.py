2490fa65c1a0c52cbe99c1d4e3d09abe78b46cab
unknown <michaelchandrashekar@gmail.com>
2025-09-01 19:32:21 +0530

    Mike

---
+++ Day1/Day1/python1.py
@@ -0,0 +1,27 @@
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+x = 5
+y = 10
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
